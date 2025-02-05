4f98ff6248fdb592912fa34a96a9e0ab18f0a4eb
HeCheng0625 <76001578+HeCheng0625@users.noreply.github.com>
2025-02-05 05:47:31 +0000

    Deploying to gh-pages from @ HeCheng0625/HeCheng0625.github.io@1860bb678bf3e2680875fc8e42e5fbb11d9d4ef6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,40 +23,26 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+        },{id: "nav-services",
+          title: "services",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
+            window.location.href = "/services/";
           },
         },{id: "dropdown-publications",
               title: "publications",
@@ -88,17 +74,6 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/blog/2024/photo-gallery/";
-        
-      },
-    },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
         
       },
     },{id: "post-a-post-with-tabs",
@@ -321,17 +296,6 @@ ninja.data = [{
           window.location.href = "/blog/2022/giscus-comments/";
         
       },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-        
-      },
     },{id: "post-a-post-with-redirect",
       
         title: "a post with redirect",
@@ -431,96 +395,60 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+    },{id: "internship-1",
+          title: '1',
+          description: "Developed on audio generation &amp; editing and larger scale text-to-speech synthesis.",
+          section: "Internship",handler: () => {
+              window.location.href = "/internship/1/";
+            },},{id: "internship-2",
+          title: '2',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "Internship",handler: () => {
+              window.location.href = "/internship/2/";
+            },},{id: "news-my-first-paper-about-audio-generation-and-editing-audit-audio-editing-by-following-instructions-with-latent-diffusion-models-got-accepted-by-neurips-2023",
+          title: '🎉 My first paper about audio generation and editing AUDIT: Audio Editing by...',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-we-released-amphion-v0-1-which-is-an-open-source-toolkit-for-audio-music-and-speech-generation",
+          title: '🔥 We released Amphion v0.1 , which is an open-source toolkit for audio,...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{
+          section: "News",},{id: "news-our-paper-factorized-diffusion-models-are-natural-and-zero-shot-speech-synthesizers-aka-naturalspeech-3-got-accepted-by-icml-2024-as-an-oral-presentation",
+          title: '🎉 Our paper Factorized Diffusion Models are Natural and Zero-shot Speech Synthesizers, aka...',
+          description: "",
+          section: "News",},{id: "news-we-released-emila-an-extensive-multilingual-and-diverse-speech-dataset-for-large-scale-speech-generation-with-101k-hours-of-speech-in-six-languages-and-features-diverse-speech-with-varied-speaking-styles",
+          title: '🔥 We released Emila: An Extensive, Multilingual, and Diverse Speech Dataset for Large-Scale...',
+          description: "",
+          section: "News",},{id: "news-our-papers-amphion-and-emila-got-accepted-by-ieee-slt-2024",
+          title: '🎉 Our papers, Amphion and Emila, got accepted by IEEE SLT 2024!',
+          description: "",
+          section: "News",},{id: "news-our-paper-sd-eval-a-benchmark-dataset-for-spoken-dialogue-understanding-beyond-words-got-accepted-by-neurips-2024",
+          title: '🎉 Our paper, SD-Eval: A Benchmark Dataset for Spoken Dialogue Understanding Beyond Words,...',
+          description: "",
+          section: "News",},{id: "news-we-released-code-2-5k-stars-in-one-week-and-checkpoints-of-maskgct-which-has-been-used-in-quwan-all-voice",
+          title: '🔥 We released code (2.5k+ stars in one week) and checkpoints of MaskGCT,...',
+          description: "",
+          section: "News",},{id: "news-maskgct-zero-shot-text-to-speech-with-masked-generative-codec-transformer-have-been-accepted-to-iclr-2025",
+          title: '🎉 MaskGCT: Zero-Shot Text-to-Speech with Masked Generative Codec Transformer have been accepted to...',
+          description: "",
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%79%75%61%6E%63%68%65%6E%67%77%61%6E%67@%6C%69%6E%6B.%63%75%68%6B.%65%64%75.%63%6E", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=60uamz4AAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://github.com/HeCheng0625", "_blank");
         },
       },{
       id: 'light-theme',
