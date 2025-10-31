c8df00bc58da206b82d64b81306367de380dcb9d
HeCheng0625 <76001578+HeCheng0625@users.noreply.github.com>
2025-10-31 08:15:20 +0000

    Deploying to gh-pages from @ HeCheng0625/HeCheng0625.github.io@3949346aa9a9b31288f2cfa05def914231e0e334 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -144,8 +144,8 @@ ninja.data = [{
           section: "News",},{id: "news-our-paper-tadicodec-text-aware-diffusion-speech-tokenizer-for-speech-language-modeling-and-metis-a-foundation-speech-generation-model-with-masked-generative-pre-training-got-accepted-by-neurips-2025",
           title: '🎉 Our paper TaDiCodec: Text-aware Diffusion Speech Tokenizer for Speech Language Modeling and...',
           description: "",
-          section: "News",},{id: "news-tadicodec-text-aware-diffusion-speech-tokenizer-for-speech-language-modeling-gets-the-onourable-mention-awards-at-nanyang-speech-technology-forum-nysf-2025-noro-noise-robust-one-shot-voice-conversion-with-hidden-speaker-representation-learning-gets-the-best-paper-finalist-at-apsipa-2025",
-          title: '🎉 TaDiCodec: Text-aware Diffusion Speech Tokenizer for Speech Language Modeling gets the onourable...',
+          section: "News",},{id: "news-tadicodec-text-aware-diffusion-speech-tokenizer-for-speech-language-modeling-gets-the-honourable-mention-awards-at-nanyang-speech-technology-forum-nysf-2025-noro-noise-robust-one-shot-voice-conversion-with-hidden-speaker-representation-learning-gets-the-best-paper-finalist-at-apsipa-2025",
+          title: '🎉 TaDiCodec: Text-aware Diffusion Speech Tokenizer for Speech Language Modeling gets the honourable...',
           description: "",
           section: "News",},{
         id: 'social-email',
